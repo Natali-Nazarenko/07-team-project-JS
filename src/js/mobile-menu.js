60645269e79fade49a5075dcd26f794813b16691
Andrii Khomenko <akhomenko1995@gmail.com>
2026-02-08 16:45:56 +0100

    add esc.key close mob-menu and no scrole with open mob-menu

--- src/js/mobile-menu.js
+++ src/js/mobile-menu.js
@@ -7,11 +7,27 @@ const toggleMenu = () => {
   menu.classList.toggle('visually-hidden');
 };
 
+const openMenu = () => {
+  toggleMenu();
+  document.body.classList.add('no-scroll');
+};
+
+const closeMenu = () => {
+  toggleMenu();
+  document.body.classList.remove('no-scroll');
+};
+
+openBtn.addEventListener('click', toggleMenu);
+closeBtn.addEventListener('click', closeMenu);
+
 mobMenuLink.forEach(link => {
   link.addEventListener('click', () => {
-    menu.classList.add('visually-hidden');
+    closeMenu();
   });
 });
 
-openBtn.addEventListener('click', toggleMenu);
-closeBtn.addEventListener('click', toggleMenu);
+document.addEventListener('keydown', esc => {
+  if (esc.key === 'Escape' && !menu.classList.contains('visually-hidden')) {
+    closeMenu();
+  }
+});
