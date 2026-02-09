d826a4990a47a3dd679302f4ad143783d019ecb1
AnnaM <amykhailova84@gmail.com>
2026-02-09 12:01:24 -0600

    add close-Btn

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,1 +1,19 @@
 import './render-functions/render-modal';
+
+const closeBtn = document.querySelector('[data-closeBtn]');
+const backdrop = document.querySelector('[data-modal]');
+
+const closeModal = () => {
+  document.body.classList.remove('modal-open');
+  backdrop.classList.remove('is-open');
+  backdrop.removeEventListener('click', onBackdropClick);
+  window.removeEventListener('keydown', onEsc);
+  closeBtn.removeEventListener('click', closeModal);
+};
+
+const onEsc = e => e.key === 'Escape' && closeModal();
+const onBackdropClick = e => e.target === backdrop && closeModal();
+
+closeBtn.addEventListener('click', closeModal);
+backdrop.addEventListener('click', onBackdropClick);
+window.addEventListener('keydown', onEsc);
