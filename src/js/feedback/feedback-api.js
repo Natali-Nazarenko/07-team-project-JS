19526cbcbc5e587bccf6f17e9d9226344bc9149f
Dvizhok <kalibry1@gmail.com>
2026-02-09 11:05:46 +0200

    fix api and css 11:05-09/02/26

--- src/js/feedback/feedback-api.js
+++ src/js/feedback/feedback-api.js
@@ -1,19 +1,21 @@
-import axios from 'axios';
-import { validInput } from '../error-handler';
+// запит на фідбеки з сервера ідуть з файла sound-wave-api.js
 
-const BASE_URL = 'https://sound-wave.b.goit.study/api';
-const END_POINT = '/feedbacks';
-const url = `${BASE_URL}${END_POINT}`;
-const option = { params: { limit: 10 } };
+// import axios from 'axios';
+// import { validInput } from '../error-handler';
 
-export async function fetchFeedbacks() {
-  try {
-    const {
-      data: { data: feedbacks },
-    } = await axios.get(url, option);
-    return Array.isArray(feedbacks) ? feedbacks : [];
-  } catch (error) {
-    validInput(error.message);
-    return [];
-  }
-}
+// const BASE_URL = 'https://sound-wave.b.goit.study/api';
+// const END_POINT = '/feedbacks';
+// const url = `${BASE_URL}${END_POINT}`;
+// const option = { params: { limit: 10 } };
+
+// export async function fetchFeedbacks() {
+//   try {
+//     const {
+//       data: { data: feedbacks },
+//     } = await axios.get(url, option);
+//     return Array.isArray(feedbacks) ? feedbacks : [];
+//   } catch (error) {
+//     validInput(error.message);
+//     return [];
+//   }
+// }
