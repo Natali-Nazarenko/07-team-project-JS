329312bc74cad6f1a4acc6c3a5d416908ca80b86
AnnaM <amykhailova84@gmail.com>
2026-02-08 18:07:47 -0600

    add modal section

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,2 +1,13 @@
 import './render-functions/render-modal';
 
+const modalEl = document.querySelector('section.modal');
+console.log(modalEl);
+
+const closeBtn = modalEl.querySelector('.modal-btn');
+console.log(closeBtn);
+
+closeBtn.addEventListener('click', modalCloseBtn);
+
+function modalCloseBtn(e) { 
+  modalEl.classList.toggle('is-open');
+};
